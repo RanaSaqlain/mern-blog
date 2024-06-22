994702ee0a75e4b1c0e8aa5fbdab06109db654b2
Muhammad Saqlain <msaqlain6666@gmail.com>
2024-06-22 17:16:58 +0500

    "Added mongoose and mongose dependencies, updated package-lock.json, and imported mongoose in api/index.js"

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"; 
 
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{console.log("Mongoose Connection Established")}).catch((err)=>{console.log(err)})
 const app = express();
 
 app.listen(3000, () => {
